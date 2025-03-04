3612037eda7b07d8e7c37c74a6ab4dff4ffc5b98
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-05 00:24:49 +0530

    Update query 8.1.sql

--- query 8.1.sql
+++ query 8.1.sql
@@ -1,16 +1,16 @@
-8. Inventory Management & Transfers
-8.1 Lost and Damaged Inventory
-Business Problem:
-Warehouse managers need to track “shrinkage” such as lost or damaged inventory to reconcile physical vs. system counts.
+-- 8. Inventory Management & Transfers
+-- 8.1 Lost and Damaged Inventory
+-- Business Problem:
+-- Warehouse managers need to track “shrinkage” such as lost or damaged inventory to reconcile physical vs. system counts.
 
-Fields to Retrieve:
+-- Fields to Retrieve:
 
-INVENTORY_ITEM_ID
-PRODUCT_ID
-FACILITY_ID
-QUANTITY_LOST_OR_DAMAGED
-REASON_CODE (Lost, Damaged, Expired, etc.)
-TRANSACTION_DATE
+-- INVENTORY_ITEM_ID
+-- PRODUCT_ID
+-- FACILITY_ID
+-- QUANTITY_LOST_OR_DAMAGED
+-- REASON_CODE (Lost, Damaged, Expired, etc.)
+-- TRANSACTION_DATE
 
 
 select
@@ -32,3 +32,8 @@ on
 	iiv.INVENTORY_ITEM_ID = ii.INVENTORY_ITEM_ID
 order by
 	ii.PRODUCT_ID
+
+It starts with the inventory_item_detail (iid) table, which logs inventory changes, and joins with inventory_item (ii) to retrieve PRODUCT_ID, 
+INVENTORY_ITEM_ID, and FACILITY_ID. The inventory_item_variance (iiv) table is included to capture quantity changes, using ABS(iiv.QUANTITY_ON_HAND_VAR) 
+to ensure values are positive.
+
