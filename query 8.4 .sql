39ce88c0599e54a0e0adaf5955b96742d34ebbd4
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 09:01:22 +0530

    Update query 8.4 .sql

--- query 8.4 .sql
+++ query 8.4 .sql
@@ -1,3 +1,15 @@
+-- 8.4 Items Where QOH and ATP Differ
+-- Business Problem:
+-- Sometimes the Quantity on Hand (QOH) doesn’t match the Available to Promise (ATP) due to pending orders, reservations, or data discrepancies. This needs review for accurate fulfillment planning.
+
+-- Fields to Retrieve:
+
+-- PRODUCT_ID
+-- FACILITY_ID
+-- QOH (Quantity on Hand)
+-- ATP (Available to Promise)
+-- DIFFERENCE (QOH - ATP)
+
 select
 	ii.PRODUCT_ID ,
 	ii.FACILITY_ID ,
