75d8807754b1f19aabbaa4d71b91381db1de3ee3
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 09:02:38 +0530

    Update query 8.6 .sql

--- query 8.6 .sql
+++ query 8.6 .sql
@@ -1,3 +1,14 @@
+-- 8.6 Total Orders by Sales Channel
+-- Business Problem:
+-- Marketing and sales teams want to see how many orders come from each channel (e.g., web, mobile app, in-store POS, marketplace) to allocate resources effectively.
+
+-- Fields to Retrieve:
+
+-- SALES_CHANNEL
+-- TOTAL_ORDERS
+-- TOTAL_REVENUE
+-- REPORTING_PERIOD
+
 SELECT 
     oh.SALES_CHANNEL_ENUM_ID,
     COUNT(oh.ORDER_ID) AS TOTAL_ORDERS,
