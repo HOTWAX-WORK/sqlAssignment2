8800205e2923eac7a734372759836f736a0e0306
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 10:21:15 +0530

    Update quert 5.2.sql

--- quert 5.2.sql
+++ quert 5.2.sql
@@ -14,38 +14,34 @@
 -- ORDER_DATE
 -- ORDER_STATUS
 
-
-
-
-
-
-
 select
-	oh.ORDER_ID ,
-	coalesce(pG.PARTY_ID, p.PARTY_ID) as PARTY_ID,
-	coalesce(pg.GROUP_NAME, p.FIRST_NAME) as CUSTOMER_NAME_FIRST_NAME,
-	p.LAST_NAME,
-	pa.ADDRESS1 as STREET_ADDRESS,
-	pa.CITY ,
-	pa.STATE_PROVINCE_GEO_ID as STATE_PROVINCE,
-	pa.POSTAL_CODE ,
-	pa.COUNTRY_GEO_ID as COUNTRY_CODE,
-	oh.STATUS_ID as ORDER_STATUS,
-	oh.ORDER_DATE
+    oh.ORDER_ID,
+    oh.ENTRY_DATE ,
+    oh.ORDER_DATE,
+    oh.STATUS_ID,
+    oh.GRAND_TOTAL,
+    p.FIRST_NAME,
+    p.LAST_NAME,
+    pa.ADDRESS1,
+    pa.ADDRESS2,
+    pa.CITY,
+    pa.STATE_PROVINCE_GEO_ID,
+    pa.POSTAL_CODE
 from
-	order_header oh
+    order_header oh
 join order_role or2 on
-	oh.ORDER_ID = or2.ORDER_ID
-	and or2.ROLE_TYPE_ID = 'BILL_TO_CUSTOMER'
-	and oh.STATUS_ID != 'ORDER_CANCELLED'
-left join party_group pg on
-	or2.PARTY_ID = pg.PARTY_ID
-left join person p on
-	p.PARTY_ID = or2.PARTY_ID
+    or2.ORDER_ID = oh.ORDER_ID
+    and or2.ROLE_TYPE_ID like '%customer'
+join person p on
+    p.PARTY_ID = or2.PARTY_ID
 join order_contact_mech ocm on
-	ocm.ORDER_ID = oh.ORDER_ID
-	and ocm.CONTACT_MECH_PURPOSE_TYPE_ID = 'SHIPPING_LOCATION'
-left join postal_address pa on
-	pa.CONTACT_MECH_ID = ocm.CONTACT_MECH_ID
+    ocm.order_id = oh.ORDER_ID
+join contact_mech cm on
+    cm.CONTACT_MECH_ID = ocm.CONTACT_MECH_ID
+join postal_address pa on
+    pa.CONTACT_MECH_ID = cm.CONTACT_MECH_ID
 where
-	oh.ORDER_DATE between '2023-10-01' and '2023-10-31'
+    pa.STATE_PROVINCE_GEO_ID = 'NY'
+group by
+    oh.order_id
+;
